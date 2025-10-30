266b8cf625532159a4b8058455b2938d2348404a
Jacob Wan <jacobwan840@gmail.com>
2025-10-30 12:06:34 -0700

    add autocommand for claude-setenv

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  -- All zfunc files
+  pattern = { "*zfunc/*" },
+  callback = function()
+    vim.bo.filetype = "zsh"
+  end,
+  desc = "Associate zfunc files with zsh",
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,12 @@
 
 -- backupcopy "yes" helps filesystem watchers to detect changes.
 vim.opt.backupcopy = "yes"
+
+-- File type overrides
+vim.filetype.add({
+  filename = {
+    -- For a specific filename (e.g., a file named 'JenkinsFile' should be Groovy)
+    -- JenkinsFile = "groovy",
+    -- ["some-filename"] = "some-filetype",
+  },
+})
